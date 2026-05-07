0bc5b3d8bc4cbc25114c1532e58513c174e27af2
Suryansh Patel <suryansh30patel@gmail.com>
2026-05-08 01:32:49 +0530

    user register done

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,6 +4,6 @@ import { registerUser, loginUser } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/logn", loginUser);
 
 export default router;
